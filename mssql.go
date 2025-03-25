b158931d5b14738f162368518482debe101d99be
Farokh <farokh.khajuee@vipps.no>
2025-03-25 14:48:54 +0100

    fixing the panic due to the concurrency issue

--- mssql.go
+++ mssql.go
@@ -11,26 +11,44 @@ import (
 	"math"
 	"net"
 	"reflect"
+	"slices"
 	"strings"
+	"sync"
 	"time"
 	"unicode"
 
 	"github.com/denisenkom/go-mssqldb/internal/querytext"
 )
 
+var registeringMutex sync.RWMutex
+
 // ReturnStatus may be used to return the return value from a proc.
 //
-//   var rs mssql.ReturnStatus
-//   _, err := db.Exec("theproc", &rs)
-//   log.Printf("return status = %d", rs)
+//	var rs mssql.ReturnStatus
+//	_, err := db.Exec("theproc", &rs)
+//	log.Printf("return status = %d", rs)
 type ReturnStatus int32
 
 var driverInstance = &Driver{processQueryText: true}
 var driverInstanceNoProcess = &Driver{processQueryText: false}
 
 func init() {
-	sql.Register("mssql", driverInstance)
-	sql.Register("sqlserver", driverInstanceNoProcess)
+	registeringMutex.Lock()
+	defer registeringMutex.Unlock()
+
+	const (
+		mssqlStr     = "mssql"
+		sqlserverStr = "sqlserver"
+	)
+	registeredDrivers := sql.Drivers()
+	if !slices.Contains(registeredDrivers, mssqlStr) {
+		sql.Register(mssqlStr, driverInstance)
+	}
+
+	if !slices.Contains(registeredDrivers, sqlserverStr) {
+		sql.Register(sqlserverStr, driverInstance)
+	}
+
 	createDialer = func(p *connectParams) Dialer {
 		return netDialer{&net.Dialer{KeepAlive: p.keepAlive}}
 	}
@@ -771,12 +789,13 @@ func (r *Rows) ColumnTypeDatabaseTypeName(index int) string {
 // not a variable length type ok should return false.
 // If length is not limited other than system limits, it should return math.MaxInt64.
 // The following are examples of returned values for various types:
-//   TEXT          (math.MaxInt64, true)
-//   varchar(10)   (10, true)
-//   nvarchar(10)  (10, true)
-//   decimal       (0, false)
-//   int           (0, false)
-//   bytea(30)     (30, true)
+//
+//	TEXT          (math.MaxInt64, true)
+//	varchar(10)   (10, true)
+//	nvarchar(10)  (10, true)
+//	decimal       (0, false)
+//	int           (0, false)
+//	bytea(30)     (30, true)
 func (r *Rows) ColumnTypeLength(index int) (int64, bool) {
 	return makeGoLangTypeLength(r.cols[index].ti)
 }
@@ -784,9 +803,10 @@ func (r *Rows) ColumnTypeLength(index int) (int64, bool) {
 // It should return
 // the precision and scale for decimal types. If not applicable, ok should be false.
 // The following are examples of returned values for various types:
-//   decimal(38, 4)    (38, 4, true)
-//   int               (0, 0, false)
-//   decimal           (math.MaxInt64, math.MaxInt64, true)
+//
+//	decimal(38, 4)    (38, 4, true)
+//	int               (0, 0, false)
+//	decimal           (math.MaxInt64, math.MaxInt64, true)
 func (r *Rows) ColumnTypePrecisionScale(index int) (int64, int64, bool) {
 	return makeGoLangTypePrecisionScale(r.cols[index].ti)
 }
